a65d400499ca2840577bb17126fa99e3c860e2ca
YI XIANG <58964880+yixiangcode@users.noreply.github.com>
2022-04-26 21:54:51 +0800

    Create ex1-4.cpp

---
+++ ex1-4.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (void) {
+     printf(" hello Selena "); 
+     printf(" my height is %d m %d cm \n",1,80);
+     printf(" this building is %5d cm \n", 890);
+     printf(" this building is %5d cm \n", 7890);
+     printf(" this building is %5d cm \n", 67890);
+     printf(" this building is %5d cm \n", 567890);
+     printf(" this building is %5d cm \n", 1234567890);
+     printf(" pi = %f \n ", 3.1415926535897932384626433832795);
+}
