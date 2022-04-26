c9e6051e48321d191549b4786db6acd50fbf6e55
YI XIANG <58964880+yixiangcode@users.noreply.github.com>
2022-04-26 21:50:03 +0800

    Create ex1-3.cpp

---
+++ ex1-3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("Hello World ");
+    
+    cout <<  "hello selena ";
+    cout <<  "my height is " << 1 << "m " << 80 << "cm";
+
+    return 0;
+}
