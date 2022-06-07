7b9563e37fe6f16925a49ece352c206e2b3de777
YI XIANG <yixiang972@gmail.com>
2022-06-07 12:58:21 +0000

    update

--- ex7-1.cpp
+++ ex7-1.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
-#include <iostream>
-using namespace std;
+#include <math.h>
 
-int main()
+int main ()
 {
-    for (int i=0; i <= 10; i+=2){
-        cout << i;
-    }
+    int i_num=4;
+    double f_num = 4.0;
+    //(double)i_num;
+    //f_num = sqrt(f_num);
+    f_num = pow(8.0, i_num);
+    printf("  %f \n",f_num);
+    printf("Square root of %lf is %lf \n",4.0,sqrt(4.0));
+    printf("Square root of %lf is %lf \n",5.0,sqrt(5.0));
 
-    return 0;
+    printf("Square root of %lf is %lf \n", 4.0, f_num);
+    return(0);
 }
