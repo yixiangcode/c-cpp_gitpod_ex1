aab3e3eb2b20fb646d0949d376b5f1ba7e653b0a
YI XIANG <yixiang972@gmail.com>
2022-06-14 13:58:04 +0000

    update

--- ex8-1.cpp
+++ ex8-1.cpp
@@ -1,6 +1,32 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int main()
-{
+int main(){
+    int b = 0 , c = 89; 
+    int a[] = {3,6,1,2,7};
+    int *p;
+    
+    printf("please b number \n");
+    scanf("%d",&b);
+    printf("  b = %d \n",b);
+    printf(" &b = %x \n",&b);
+    
+    p = &b;
+    printf("  b = %d \n",*p);
+    printf(" &b = %x \n",p);
+    
+    printf(" a[0] = %d \n",a[0]);
+    printf(" a[1] = %d \n",a[1]);
+    printf("&a[0] = %x \n",&a[0]);
+    printf("&a[0] = %x \n",a);
+    p = a;
+    printf(" a[0] = %d \n",*p);
+    printf(" a[1] = %d \n",*(p+1));
+
+    for(i=0;i < n;i++){
+        printf(" a[%d] = %d \n",i,*(p+i));
+        printf(" a[%d] = %d \n",i,  p[i]);
+    }
+
     return 0;
 }
