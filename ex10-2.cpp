9f8582e5d1b084d8c55d8e61309ecd958e690f52
YI XIANG <yixiang972@gmail.com>
2022-06-28 13:34:23 +0000

    10

--- ex10-2.cpp
+++ ex10-2.cpp
@@ -5,6 +5,7 @@ int main(){
     char s[] = "Hello world ! Are you in Taiwan ?";
     int i;
     int n = sizeof(s)/sizeof(char),n_i;
+    char* ss;
 
     for (i = 0; s[i] != '\0'; ++i);
     n_i = i;
